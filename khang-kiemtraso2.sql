fe217f918edf4afae4e33846711ee56bdab9da6b
nbkdept123 <khangnb6102004@gmail.com>
2024-10-24 15:27:37 +0700

    Add files via upload

---
+++ khang-kiemtraso2.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE db_Nguyen_Ba_Khang;
+USE db_Nguyen_Ba_Khang;
+
+CREATE TABLE Course (
+    Id INT PRIMARY KEY,
+    Tittle VARCHAR(255),
+    Description TEXT,
+    ImageUrl VARCHAR(255)
+);
